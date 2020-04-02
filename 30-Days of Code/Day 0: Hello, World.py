0755468fa614419ce5d00c53842be1d199844d6b
puneetrohtela <58034273+puneetrohtela@users.noreply.github.com>
2020-04-02 13:57:18 +0530

    Update and rename Day 0: Hello, World. to Day 0: Hello, World.py

---
+++ 30-Days of Code/Day 0: Hello, World.py
@@ -0,0 +1,7 @@
+# Hi Everyone,In this series i am going to upload all the program of 30 Days of Code in python.
+
+# In This program we are going to print inputfrom user as string.
+
+input_string = input()
+print('Hello, World.')
+print(input_string)
